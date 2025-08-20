de71e1fe7ac3ad07c1bfbcec6bc7e7a01471e18e
sch1afend <cosminflorea_2108@yahoo.com>
2025-08-20 19:04:06 +0300

    view

--- src/components/common/(modals)/modal.tsx
+++ src/components/common/(modals)/modal.tsx
@@ -1,8 +1,8 @@
 // this is used for details view for each led in the list
 
 'use client';
-import { ReactNode } from 'react';
 import { X } from 'lucide-react';
+import { ReactNode } from 'react';
 
 interface ModalProps {
   isOpen: boolean;
@@ -14,13 +14,13 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
-      <div className="relative w-2/4 h-2/4 overflow-auto rounded-xl bg-neutral-100 p-6 shadow-lg">
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm'>
+      <div className='relative h-2/4 w-2/4 overflow-auto rounded-xl bg-neutral-100 p-6 shadow-lg'>
         <button
-          className="absolute left-4 top-4 text-gray-500 hover:text-gray-800"
+          className='absolute top-4 left-4 text-gray-500 hover:text-gray-800'
           onClick={onClose}
         >
-          <X className="h-6 w-6" />
+          <X className='h-6 w-6' />
         </button>
         {children}
       </div>
